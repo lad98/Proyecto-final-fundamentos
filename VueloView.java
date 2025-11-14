1fbd162ddee8889fde4a2c14fe24119c304d4c82
lad98 <ldanielajimenezm@gmail.com>
2025-11-14 16:52:37 -0500

    Fix formatting and add missing newline at end of file

--- VueloView.java
+++ VueloView.java
@@ -1,3 +1,18 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+import java.util.Date;
+import java.util.Calendar;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
