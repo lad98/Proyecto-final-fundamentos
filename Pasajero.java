fff5e31b7671c8effc0e98bf843a6754869d4c08
lad98 <ldanielajimenezm@gmail.com>
2025-11-14 16:28:39 -0500

    Refactor Pasajero class package and add serialVersionUID
    Updated package name and added serialVersionUID.

--- Pasajero.java
+++ Pasajero.java
@@ -1,9 +1,10 @@
-package modeloDTO;
+package modelo.dto;
 
 import java.io.Serializable;
 
 public class Pasajero implements Serializable {
-    private String nombre;
+    private static final long serialVersionUID = 1L;
+	private String nombre;
     private String correoLectronico;
 
     public String getNombre() {
@@ -13,4 +14,14 @@ public class Pasajero implements Serializable {
     public String getCorreoLectronico() {
         return correoLectronico;
     }
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setCorreoLectronico(String correoLectronico) {
+		this.correoLectronico = correoLectronico;
+	}
+    
+    
 }
