22aa5f8dcf28bd8ec177991605e07a624e6b8e5a
lad98 <ldanielajimenezm@gmail.com>
2025-11-16 19:18:45 -0500

    Add Agencia class with main method and controllers

---
+++ Agencia.java
@@ -0,0 +1,12 @@
+package agencia;
+
+import controlador.ControladorVuelo;
+import vista.VueloView;
+
+public class Agencia {
+	    public static void main(String[] args) {
+	    ControladorVuelo vueloController = new ControladorVuelo(new VueloView()) ;
+	        
+	    }
+	}
+
