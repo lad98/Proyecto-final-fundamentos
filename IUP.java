fbba483e416b226a6e949c0ee40fb613b2fe2254
IsabellaAgudelo01 <isabella.agudelo01@usa.edu.co>
2025-11-22 12:35:12 -0500

    Create IUP class for main application window

---
+++ IUP.java
@@ -0,0 +1,54 @@
+package Vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class IUP extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	public JMenuItem vuelo;
+	public JMenuItem asiento;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IUP frame = new IUP();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IUP() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		vuelo = new JMenuItem("Vuelo");
+		menuBar.add(vuelo);
+		
+		asiento = new JMenuItem("Asignar Asiento");
+		menuBar.add(asiento);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+	}
+}
